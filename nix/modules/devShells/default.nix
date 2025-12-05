6e2c87b792dd761593e275c12434e37e9321864f
Qiming Chu <cchuqiming@gmail.com>
2025-12-05 20:27:53 +0800

    chore: reformat
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- nix/modules/devShells/default.nix
+++ nix/modules/devShells/default.nix
@@ -1,8 +1,14 @@
 { pkgs, config, ... }:
 {
   devShells.default = pkgs.mkShell {
-    buildInputs = with pkgs; [
-      mill
-    ];
+    buildInputs =
+      with pkgs;
+      [
+        mill
+        yosys
+      ]
+      ++ pkgs.lib.optionals (!pkgs.stdenv.hostPlatform.isDarwin) [
+        iEDA
+      ];
   };
 }
