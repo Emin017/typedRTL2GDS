7ecff6580f2acb14076fe97b102e23b311029368
Emin <me@emin.chat>
2025-12-05 10:31:46 +0800

    typedRTL2GDS: set foundryEnv in GlobalConfigs
    Signed-off-by: Emin <me@emin.chat>

--- typedRTL2GDS/src/GlobalConfigs.scala
+++ typedRTL2GDS/src/GlobalConfigs.scala
@@ -12,11 +12,24 @@ trait GlobalConfigs {
 
   def iEDAScriptsPath: String = s"${scriptsPath}/ieda"
 
-  def genCommonEnv[T <: FlowContext](i: T) = Seq(
-    "IEDA_CONFIG_DIR" -> s"$iEDAScriptsPath/iEDA_config",
-    "IEDA_TCL_SCRIPT_DIR" -> s"$iEDAScriptsPath/script",
-    "RESULT_DIR" -> i.config.resultDir,
-    "OUTPUT_DEF" -> i.outputCtx.defPath.map(_.value).getOrElse(""),
-    "OUTPUT_VERILOG" -> i.outputCtx.verilogFile.map(_.value).getOrElse("")
-  )
+  def genCommonEnv[T <: FlowContext](i: T) = {
+    val foundryEnv = if (i.config.foundry.name == "ics55") {
+      val pdkDir = i.config.foundry.pdkDir
+      Seq(
+        "TECH_LEF" -> s"$pdkDir/prtech/techLEF/N551P6M_ieda.lef",
+        "LEF_STDCELL" -> s"$pdkDir/IP/STD_cell/ics55_LLSC_H7C_V1p10C100/ics55_LLSC_H7CL/lef/ics55_LLSC_H7CL_ieda.lef",
+        "LIB_STDCELL" -> s"$pdkDir/IP/STD_cell/ics55_LLSC_H7C_V1p10C100/ics55_LLSC_H7CL/liberty/ics55_LLSC_H7CL_typ_tt_1p2_25_nldm.lib"
+      )
+    } else {
+      Seq.empty
+    }
+
+    Seq(
+      "IEDA_CONFIG_DIR" -> s"$iEDAScriptsPath/iEDA_config",
+      "IEDA_TCL_SCRIPT_DIR" -> s"$iEDAScriptsPath/script",
+      "RESULT_DIR" -> i.config.resultDir,
+      "OUTPUT_DEF" -> i.outputCtx.defPath.map(_.value).getOrElse(""),
+      "OUTPUT_VERILOG" -> i.outputCtx.verilogFile.map(_.value).getOrElse("")
+    ) ++ foundryEnv
+  }
 }
